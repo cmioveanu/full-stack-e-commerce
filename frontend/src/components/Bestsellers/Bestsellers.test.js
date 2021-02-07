0148e9914cb5d5b6c6938a2f222092fa3c14176a
Cristian <cmioveanu@gmail.com>
2021-02-07 01:37:38 +0000

    add tests

--- frontend/src/components/Bestsellers/Bestsellers.test.js
+++ frontend/src/components/Bestsellers/Bestsellers.test.js
@@ -1,4 +1,17 @@
 import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Bestsellers } from './Bestsellers';
 
+test('renders Bestsellers component', () => {
+    render(<Bestsellers />);
 
+    const bestsellersContainer = document.querySelector('.bestsellers');
+    expect(bestsellersContainer).toBeInTheDocument();
+});
+
+test('renders Bestsellers component', async () => {
+    render(<Bestsellers />);
+
+    
+});
 
